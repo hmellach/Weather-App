452bde1054406b2587eb24848cd5f1bc316b5f2d
Harika Mellacheruvu <64932392+hmellach@users.noreply.github.com>
2023-08-16 15:59:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
-    const APIKey = '17d09e30dba90c45ffa6172834993de9';
+    const APIKey = 'Your API Key';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '') 
